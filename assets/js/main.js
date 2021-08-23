7ee9c8375573d016e7776b6505fed9aaed4d99aa
jasmine <nikolyanhasmik@mail.ru>
2021-08-23 19:57:58 +0400

    add jquery functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
-jQuery(function () {
-    console.log('test')
+// jQuery(function () {
+//     console.log('test')
+// })
+// $().ready(function () {
+//
+// })
+
+$(document).ready(function () {
+    // $('#content').click(function () {
+    //     alert('Hello')
+    // })
+    $('#content ul li').on('click', function () {
+        alert('ddd');
+    })
+    $('#content ul').children('li').each(function () {
+        $(this).css('color', 'red')
+        $(this).text('hello')
+    })
 })
